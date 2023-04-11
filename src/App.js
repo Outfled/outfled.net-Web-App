f37e2e295b2ff3359328a2713dc6f51a867195bf
Outfled <OutfledFTW@gmail.com>
2023-04-11 09:01:20 -0400

    a

--- src/App.js
+++ src/App.js
@@ -11,28 +11,31 @@ title.innerText = 'Outfled';
 async function myFunc() {
     const file = await Storage.get("640x800_21ee17ea-ab0a-44ca-8285-9cfd33a794f9.jpg");
 }
+function GetUrl(){
+    const image = myFunc().then(string => {
+        return value;
+    });
+}
 
 function App() {
-    const image = myFunc().then(string => {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        In development.
-                    </p>
-                    <a
-                        className="App-link"
-                        href={image}
-                        target="_blank"
-                        rel='noreferrer'
-                    >
-                        Link Tree
-                    </a>
-                </header>
-            </div>
-        );
-    })
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    In development.
+                </p>
+                <a
+                    className="App-link"
+                    href={image}
+                    target="_blank"
+                    rel='noreferrer'
+                >
+                    Link Tree
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
