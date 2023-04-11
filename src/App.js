5aa069879d97ac320e9fd3300befb38ddc3aaf06
Outfled <OutfledFTW@gmail.com>
2023-04-11 05:41:05 -0400

    a

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ var title = document.querySelector('title');
 title.innerText = 'Outfled';
 
 async function myFunc() {
-    const file = await Storage.get("640x800_21ee17ea-ab0a-44ca-8285-9cfd33a794f9.jpg", {
-        level: "public",
-        download: true
-    });
+    const file = await Storage.get("640x800_21ee17ea-ab0a-44ca-8285-9cfd33a794f9.jpg");
     return file;
 }
 const image = myFunc();
@@ -29,7 +26,7 @@ function App() {
                     className="App-link"
                     href={image}
                     target="_blank"
-                    rel="noopener noreferrer"
+                    rel='noreferrer'
                 >
                     Link Tree
                 </a>
