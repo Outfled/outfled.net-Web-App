198665e7b297ee88702cf23e59afe838a4f07c70
Outfled <55547083+Outfled@users.noreply.github.com>
2023-04-26 05:00:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { Amplify, Storage } from 'aws-amplify';
-import awsconfig from './aws-exports';
+//import { Amplify, Storage } from 'aws-amplify';
+//import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+//Amplify.configure(awsconfig);
 
 var title = document.querySelector('title');
 title.innerText = 'Outfled';
 
-async function myFunc() {
-    const file = await Storage.get("640x800_21ee17ea-ab0a-44ca-8285-9cfd33a794f9.jpg");
-}
-function GetUrl(){
-    const image = myFunc().then(string => {
-        return value;
-    });
-}
+//async function myFunc() {
+//    const file = await Storage.get("640x800_21ee17ea-ab0a-44ca-8285-9cfd33a794f9.jpg");
+//}
+//function GetUrl(){
+//    const image = myFunc().then(string => {
+//        return value;
+//    });
+//}
 
 function App() {
-    const image = GetUrl();
     return (
-        <div className="App">
+        <div className="OutfledApp">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
