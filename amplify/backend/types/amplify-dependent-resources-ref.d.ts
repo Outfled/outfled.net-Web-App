5d34d140dbc9616070001df6d599bec4fc793818
Outfled <OutfledFTW@gmail.com>
2023-05-02 00:46:29 -0400

    added storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -25,5 +25,11 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     }
+  },
+  "storage": {
+    "OutfledLatestAppsBucketResource": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
